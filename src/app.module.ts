3645addadd31d3d95fc1ebaa439dbb5f1c8dd83f
amiriar <amiriar.irr@gmail.com>
2024-11-06 05:26:19 +0330

    decorators and exceptions added

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { TransactionsModule } from './module/transactions/transactions.module';
 import { BooksModule } from './module/admin/books/books.module';
 import { UsersModule } from './module/admin/users/users.module';
 import { TransactionsModule } from './module/admin/transactions/transactions.module';
+import { AuthModule } from './module/auth/auth.module';
 
 @Module({
-  imports: [BooksModule, UsersModule, TransactionsModule],
+  imports: [BooksModule, UsersModule, TransactionsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
