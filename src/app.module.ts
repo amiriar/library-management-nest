8815a9292fb9181988059d242f98307159be7c53
amiriar <amiriar.irr@gmail.com>
2024-11-06 05:22:19 +0330

    all modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './module/books/books.module';
+import { UsersModule } from './module/users/users.module';
+import { TransactionsModule } from './module/transactions/transactions.module';
+import { BooksModule } from './module/admin/books/books.module';
+import { UsersModule } from './module/admin/users/users.module';
+import { TransactionsModule } from './module/admin/transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [BooksModule, UsersModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
